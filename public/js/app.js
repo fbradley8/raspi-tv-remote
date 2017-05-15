d8b4e31605de81b7aed946142d59d8ed533628ac
Forrest Bradley <fbradley8@gmail.com>
2017-05-15 01:45:41 -0700

    update to use new pi zero

--- public/js/app.js
+++ public/js/app.js
@@ -2,8 +2,7 @@ $(function() {
     $('*[clickable]').each(function(index, element) {
         var self = $(element);
         self.click(function(e) {
-            // chrome.runtime.sendMessage({key: self.attr('id')});
-            $.ajax('http://raspberrypi:8080/press/' + self.attr('id'))
+            $.ajax('http://pizero:8080/press/' + self.attr('id'))
         });
     });
 });
